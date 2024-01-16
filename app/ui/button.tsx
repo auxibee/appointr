2abde8a41a093e5d041c5c63717a774c15be2daa
Yaw Twumasi Acheampong <auxibee@gmail.com>
2024-01-15 19:35:45 -0500

    refactor button to make it reusable

---
+++ app/ui/button.tsx
@@ -0,0 +1,19 @@
+import clsx from 'clsx';
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+}
+
+export function Button({ children, className, ...rest }: ButtonProps) {
+  return (
+    <button
+      {...rest}
+      className={clsx(
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center",
+        className,
+      )}
+    >
+      {children}
+    </button>
+  );
+}
